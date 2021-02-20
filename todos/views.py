ee277176c935895efccfbcb36aa74159699cfac6
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2021-02-21 06:03:34 +0900

    redirect after add todo

--- todos/views.py
+++ todos/views.py
@@ -1,18 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import addForm
 from .models import Todo
 
 
 def add(request):
     form = addForm(request.POST)
-    form_title = 'Add Todo'
-    confirm_message = None
     if form.is_valid():
         title = form.cleaned_data['title']
         description = form.cleaned_data['description']
         is_completed = form.cleaned_data['is_completed']
         todo = Todo(title=title, description=description, is_completed=is_completed)
         todo.save()
-        confirm_message = 'New Todo added'
-        form = None
-    return render(request, 'todos/add.html', {'form_title': form_title, 'form': form, 'confirm_message': confirm_message})
+        return redirect('/todos')
+    return render(request, 'todos/add.html', {'form': form})
